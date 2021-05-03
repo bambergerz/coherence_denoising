b52f18432d84ad7ff97c6f7e5a791d7e65598f80
nv78 <vidranatan@gmail.com>
2021-05-02 21:52:20 -0700

    new data

--- loading.py
+++ loading.py
@@ -2,6 +2,6 @@ import pandas as pd
 import os
 import re
 
-Data = pd.read_csv('./WikiHow-Dataset/all_train.txt')
+Data = pd.read_csv('./WikiHow-Dataset/wikihowAll.csv')
 Data = Data.astype(str)
 print(Data.head())
